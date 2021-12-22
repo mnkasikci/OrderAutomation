25528a320c30f01d39905c339cad1df29ff9ef14
mnkasikci <74023838+mnkasikci@users.noreply.github.com>
2021-12-22 22:35:03 +0300

    Fixed ignored files

---
+++ OrderAutomation/DBHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderAutomation
+{
+    public static class DBHelper
+    {
+
+        private const string connectionString = "Data Source=localhost;Initial Catalog=OrderAutomation;Integrated Security=True";
+        private static SqlConnection Singleton;
+
+        public static SqlConnection GetConnectionItem()
+        {
+            if(Singleton==null)
+                Singleton = new SqlConnection(connectionString);
+            return Singleton;
+        }
+        
+    }
+}
