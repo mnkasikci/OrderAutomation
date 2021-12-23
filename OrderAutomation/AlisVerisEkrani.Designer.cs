06a2d61b0138482b7ff9e893e4668efa320a325c
ONUR KAYA <onutkaya@gmail.com>
2021-12-23 19:35:16 +0300

    duzenlenen alısverisekrani

--- OrderAutomation/AlisVerisEkrani.Designer.cs
+++ OrderAutomation/AlisVerisEkrani.Designer.cs
@@ -30,20 +30,21 @@ namespace OrderAutomation
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.urunID = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.urunIsmi = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.urunFiyat = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.label1 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.label4 = new System.Windows.Forms.Label();
             this.labelkdvsiz = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
             this.labelkdvli = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.urunID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.urunIsmi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.urunFiyat = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -54,43 +55,12 @@ namespace OrderAutomation
             this.urunID,
             this.urunIsmi,
             this.urunFiyat});
-            this.dataGridView1.Location = new System.Drawing.Point(3, 24);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Location = new System.Drawing.Point(3, 32);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 29;
-            this.dataGridView1.Size = new System.Drawing.Size(381, 262);
+            this.dataGridView1.Size = new System.Drawing.Size(435, 349);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // urunID
-            // 
-            this.urunID.HeaderText = "Column1";
-            this.urunID.MinimumWidth = 6;
-            this.urunID.Name = "urunID";
-            this.urunID.Width = 125;
-            // 
-            // urunIsmi
-            // 
-            this.urunIsmi.HeaderText = "Column1";
-            this.urunIsmi.MinimumWidth = 6;
-            this.urunIsmi.Name = "urunIsmi";
-            this.urunIsmi.Width = 125;
-            // 
-            // urunFiyat
-            // 
-            this.urunFiyat.HeaderText = "Column1";
-            this.urunFiyat.MinimumWidth = 6;
-            this.urunFiyat.Name = "urunFiyat";
-            this.urunFiyat.Width = 125;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(157, 7);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 15);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "urunler";
             // 
             // comboBox1
             // 
@@ -103,36 +73,35 @@ namespace OrderAutomation
             "5",
             "6",
             "7"});
-            this.comboBox1.Location = new System.Drawing.Point(388, 214);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox1.Location = new System.Drawing.Point(443, 285);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(133, 23);
+            this.comboBox1.Size = new System.Drawing.Size(151, 28);
             this.comboBox1.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(436, 181);
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(612, 312);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(30, 15);
+            this.label2.Size = new System.Drawing.Size(172, 25);
             this.label2.TabIndex = 3;
-            this.label2.Text = "adet";
+            this.label2.Text = "KDV\'li toplam fiyat:";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(333, 161);
+            this.label3.Location = new System.Drawing.Point(381, 215);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(0, 15);
+            this.label3.Size = new System.Drawing.Size(0, 20);
             this.label3.TabIndex = 4;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(405, 264);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Location = new System.Drawing.Point(463, 352);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(92, 22);
+            this.button1.Size = new System.Drawing.Size(105, 29);
             this.button1.TabIndex = 5;
             this.button1.Text = "Sepete Ekle";
             this.button1.UseVisualStyleBackColor = true;
@@ -141,78 +110,119 @@ namespace OrderAutomation
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(558, 24);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(616, 32);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(132, 154);
+            this.listBox1.Size = new System.Drawing.Size(150, 204);
             this.listBox1.TabIndex = 6;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(558, 237);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(108, 15);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "KDV\'li toplam fiyat:";
             // 
             // labelkdvsiz
             // 
             this.labelkdvsiz.AutoSize = true;
-            this.labelkdvsiz.Location = new System.Drawing.Point(672, 200);
+            this.labelkdvsiz.Location = new System.Drawing.Point(788, 267);
             this.labelkdvsiz.Name = "labelkdvsiz";
-            this.labelkdvsiz.Size = new System.Drawing.Size(30, 15);
+            this.labelkdvsiz.Size = new System.Drawing.Size(38, 20);
             this.labelkdvsiz.TabIndex = 8;
             this.labelkdvsiz.Text = "fiyat";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(558, 200);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(92, 15);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "urunler toplamı:";
             // 
             // labelkdvli
             // 
             this.labelkdvli.AutoSize = true;
-            this.labelkdvli.Location = new System.Drawing.Point(683, 237);
+            this.labelkdvli.Location = new System.Drawing.Point(790, 316);
             this.labelkdvli.Name = "labelkdvli";
-            this.labelkdvli.Size = new System.Drawing.Size(57, 15);
+            this.labelkdvli.Size = new System.Drawing.Size(71, 20);
             this.labelkdvli.TabIndex = 10;
             this.labelkdvli.Text = "fiyat+kdv";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(631, 279);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Location = new System.Drawing.Point(721, 372);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(92, 22);
+            this.button2.Size = new System.Drawing.Size(105, 29);
             this.button2.TabIndex = 11;
             this.button2.Text = "Sepeti Boşalt";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // urunID
+            // 
+            this.urunID.HeaderText = "urunID";
+            this.urunID.MinimumWidth = 6;
+            this.urunID.Name = "urunID";
+            this.urunID.Width = 125;
+            // 
+            // urunIsmi
+            // 
+            this.urunIsmi.HeaderText = "urunIsmi";
+            this.urunIsmi.MinimumWidth = 6;
+            this.urunIsmi.Name = "urunIsmi";
+            this.urunIsmi.Width = 125;
+            // 
+            // urunFiyat
+            // 
+            this.urunFiyat.HeaderText = "urunFiyat";
+            this.urunFiyat.MinimumWidth = 6;
+            this.urunFiyat.Name = "urunFiyat";
+            this.urunFiyat.Width = 125;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(612, 262);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(154, 25);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Ürünler toplamı :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(512, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 25);
+            this.label5.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(491, 248);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 25);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "ADET";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(155, 4);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 25);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "ÜRÜNLER";
+            // 
             // AlisVerisEkrani
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(797, 338);
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(911, 451);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.labelkdvli);
-            this.Controls.Add(this.label5);
             this.Controls.Add(this.labelkdvsiz);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "AlisVerisEkrani";
             this.Text = "AlisVerisEkrani";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -225,20 +235,21 @@ namespace OrderAutomation
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label labelkdvsiz;
+        private System.Windows.Forms.Label labelkdvli;
+        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.DataGridViewTextBoxColumn urunID;
         private System.Windows.Forms.DataGridViewTextBoxColumn urunIsmi;
         private System.Windows.Forms.DataGridViewTextBoxColumn urunFiyat;
+        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label labelkdvli;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
     }
 }
 
