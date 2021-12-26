66c8bb4d064f22c21aaa4e5d38b4374eaeb35702
ONUR KAYA <onutkaya@gmail.com>
2021-12-26 19:35:52 +0300

    proje teslim ( UML eklendi )

---
+++ SQLQuery1.sql
@@ -0,0 +1,5 @@
+
+insert into [User] ([UserName],[Password],[Name],[Address],[UserTypeId])
+values ('Ahmet99','123456','Ahmet','Turgutlu',2)
+select * from [User]
+delete from [user] where [UserName] = 'Ahmet99'
